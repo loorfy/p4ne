9bf23e46b157f98d3ec3ff28f0762ecb9fd38b95
loorfy <bgrmayak@mail.ru>
2023-03-21 18:08:03 +0300

    added lab1.2

---
+++ lab1.2/lab1_2.py
@@ -0,0 +1,16 @@
+from matplotlib import pyplot
+from openpyxl import load_workbook
+
+wb = load_workbook('data_analysis_lab.xlsx')
+sheet = wb['Data']
+
+def get_value(x):
+    return x.value
+
+list_of_years = list(map(get_value, sheet['A'][1:]))
+list_of_temperature = list(map(get_value, sheet['C'][1:]))
+list_of_activity = list(map(get_value, sheet['D'][1:]))
+
+pyplot.plot(list_of_years, list_of_temperature)
+pyplot.plot(list_of_years, list_of_activity)
+pyplot.show()
