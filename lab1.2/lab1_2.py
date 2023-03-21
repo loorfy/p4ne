c0e803fa7014075f42aae59dd889f05d579f999f
loorfy <bgrmayak@mail.ru>
2023-03-21 18:20:40 +0300

    added labels to lab1.2

--- lab1.2/lab1_2.py
+++ lab1.2/lab1_2.py
@@ -11,6 +11,6 @@ list_of_years = list(map(get_value, sheet['A'][1:]))
 list_of_temperature = list(map(get_value, sheet['C'][1:]))
 list_of_activity = list(map(get_value, sheet['D'][1:]))
 
-pyplot.plot(list_of_years, list_of_temperature)
-pyplot.plot(list_of_years, list_of_activity)
+pyplot.plot(list_of_years, list_of_temperature, label='first')
+pyplot.plot(list_of_years, list_of_activity, label='second')
 pyplot.show()
